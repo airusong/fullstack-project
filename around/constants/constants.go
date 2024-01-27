c86a345279dcbfe2ff34811508c48b92d9cfb819
airusong <yinglaisong@around.us-central1-a.c.ringed-index-412204.internal>
2024-01-27 19:26:31 +0000

    init ES backend and search function

---
+++ around/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://34.135.97.38:9200"
+	ES_USERNAME = "airusong"
+	ES_PASSWORD = "airusong"
+)
--- around/main.go
+++ around/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 
+	"around/backend"
 	"around/handler"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("started-service")
+	backend.InitElasticsearchBackend()
 	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
 }
